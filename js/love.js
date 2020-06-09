0e64b4f98d47ea6383e8f1408c868c637738f765
wangfanrui <wangfanrui02@fehorizon.com>
2020-06-09 10:40:48 +0800

    ''

--- js/love.js
+++ js/love.js
@@ -128,6 +128,7 @@ function Rise() {
 	
 
 }
+
 function typing () {
 	if (strIndex <= text.length) {
 	  textBox.innerHTML = text.slice(0, strIndex++) + '_'
@@ -139,13 +140,77 @@ function typing () {
 	}
 }
 
+var iBase = {
+	Id: function(name){
+		return document.getElementById(name);
+	},
+//设置元素透明度,透明度值按IE规则计,即0~100
+	SetOpacity: function(ev, v){
+		ev.filters ? ev.style.filter = 'alpha(opacity=' + v + ')' : ev.style.opacity = v / 100;
+	}
+}
+
+//淡出效果(含淡出到指定透明度)
+function fadeOut(elem, speed, opacity){
+/*
+* 参数说明
+* elem==>需要淡入的元素
+* speed==>淡入速度,正整数(可选)
+* opacity==>淡入到指定的透明度,0~100(可选)
+*/
+   speed = speed || 20;
+   opacity = opacity || 0;
+   //初始化透明度变化值为0
+   var val = 100;
+//循环将透明值以5递减,即淡出效果
+   (function(){
+       iBase.SetOpacity(elem, val);
+       val -= 5;
+       if (val >= opacity) {
+           setTimeout(arguments.callee, speed);
+       }else if (val < 0) {
+//元素透明度为0后隐藏元素
+           elem.style.display = 'none';
+       }
+   })();
+}
+//淡入效果(含淡入到指定透明度)
+function fadeIn(elem, speed, opacity){
+/*
+* 参数说明
+* elem==>需要淡入的元素
+* speed==>淡入速度,正整数(可选)
+* opacity==>淡入到指定的透明度,0~100(可选)
+*/
+   speed = speed || 20;
+   opacity = opacity || 100;
+//显示元素,并将元素值为0透明度(不可见)
+   elem.style.display = 'block';
+   iBase.SetOpacity(elem, 0);
+//初始化透明度变化值为0
+   var val = 0;
+//循环将透明值以5递增,即淡入效果
+   (function(){
+       iBase.SetOpacity(elem, val);
+       val += 5;
+       if (val <= opacity) {
+           setTimeout(arguments.callee, speed)
+       }
+   })();
+}
+function showModel(str) {
+	document.getElementById('img').src=str
+	fadeIn(document.getElementById('myModel'),20)
+}
+function closeModel() {
+	fadeOut(document.getElementById('myModel'),20)
+}
 window.onload = function () {
     setTimeout(() => {
-
         timer = setInterval(() => {
             Next();
         }, 300);
 
-
-    }, 12000);   //gif图播放完毕所需时间为11.73s
+    }, 12000); 
+	//gif图播放完毕所需时间为11.73s
 };
